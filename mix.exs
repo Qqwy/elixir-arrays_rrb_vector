49b6617732892ab96567b6dee1339830e10ad01f
Marten/Qqwy <w-m@wmcode.nl>
2022-05-27 15:00:21 +0200

    Update version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ArraysRRBVector.MixProject do
   def project do
     [
       app: :arrays_rrb_vector,
-      version: "0.2.0",
+      version: "0.3.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
